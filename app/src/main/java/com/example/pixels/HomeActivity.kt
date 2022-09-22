9f0c4c772cfbbc9ad949b35ec9ab08eafe7c0b8c
abhaymaurya <abhay10june1999@gmail.com>
2022-09-23 01:18:51 +0530

    navigation added at home screen.

--- app/src/main/java/com/example/pixels/HomeActivity.kt
+++ app/src/main/java/com/example/pixels/HomeActivity.kt
@@ -12,12 +12,13 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.bottomNavigationView.menu.findItem(R.id.notes).setChecked(true)
+
         binding.bottomNavigationView.setOnNavigationItemSelectedListener {
             when (it.itemId) {
                 R.id.notes -> showToast("Notes clicked")
                 R.id.my_books -> showToast("My books clicked")
                 R.id.download_books -> showToast("Download book clicked")
-
             }
             true
         }
